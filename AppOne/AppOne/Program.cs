3ee40c1e0eb0906ecdbf1171bc22dcc069320892
charitraa <stharabi9862187405@gmail.com>
2024-09-13 11:59:44 +0545

    added the folders

---
+++ AppOne/AppOne/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, people World!");
+        }
+    }
+}
